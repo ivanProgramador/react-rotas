339e0bc7f7771737dc5a62b84f45c6acb8580b23
ivan <ivan-guita@hotmail.com>
2023-12-16 18:10:09 -0300

    estrutura basica do mnu completa

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import Usuarios from './components/Usuarios/Usuarios'
 function App() {
   return (
     <div className="App">
+      <header>
+         <nav>
+           <ul>
+             <li><a href='/'>Inicio</a></li>
+             <li><a href='/usuarios'>Usuarios cadastrados</a></li>
+             <li><a href='/adicionar'>Adicionar usuario</a></li>
+           </ul>
+         </nav>
+      </header>
       <main>
         <Usuarios />
       </main>
